1f4fc367b89f2965da8ee9476adfcfb0f265e073
Jeff Mesnil <jmesnil@gmail.com>
2017-02-28 17:53:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # microprofile-config-example
-Web app example using the Eclipse Microprofile Config API
+
+This project defines a Web app that uses the Eclipse Microprofile Config API to read its configuration.
+
+It is deployed in WildFly and uses its microprofile-config Susbystem extension.
+
+# Instructions
+
+To run the example, you need:
+
+1. Compile and deploy the [microprofile-config](https://github.com/jmesnil/microprofile-config-extension) extension in WildFly
+2. Add the extension and subsystem to WildFly:
+
+````
+/extension=net.jmesnil.microprofile-config-extension:add()
+/subsystem=microprofile-config:add()
+````
+
+3. Package this Web app and deploy it in WildFly:
+
+````
+mvn clean package deploy
+````
+
+4. Go to http://localhost:8080/microprofile-config-example/config/all to see all the properties that are accessed by the Web app.
+
+They include:
+ * environment properties
+ * System properties
+ * Properties from the src/main/resources/META-INF/microprofile-config.properties file
+
+
---
+++ src/main/java/net/jmesnil/microprofile/config/example/HelloWorld.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.jmesnil.microprofile.config.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+/**
+ * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
+ * is enabled
+ *
+ * @author gbrey@redhat.com
+ *
+ */
+
+@Path("/")
+public class HelloWorld {
+    @GET
+    @Path("/all")
+    public String getAllProperties() {
+        Config config = ConfigProvider.getConfig();
+        StringBuilder out = new StringBuilder();
+
+        out.append("<h2>ConfigSources</h2>");
+        out.append("<ul>");
+        for (ConfigSource source : config.getConfigSources()) {
+            out.append("<li>" + source.getId() + " - " + source.getOrdinal() + "</li>");
+        }
+        out.append("</ul>");
+
+        out.append("<h2>Properties</h2>");
+        out.append("<ul>");
+        for (String name : config.getPropertyNames()) {
+            out.append(String.format("<li>%s = %s</li>\n", name, config.getString(name).get()));
+        }
+        out.append("</ul>");
+        return out.toString();
+    }
+}
